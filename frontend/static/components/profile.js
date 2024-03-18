079a4580e0e5426345788f8a48f0cc71864961a1
Santhosh G S <santhoshgs013@gmail.com>
2024-03-19 00:15:08 +0530

    Error in the profile.js, not working

--- frontend/static/components/profile.js
+++ frontend/static/components/profile.js
@@ -2,8 +2,38 @@ const profile = {
   template: `
   <div>
     <h1>Profile</h1>
-    <p>This is the profile page</p>
-  </div>`
+    <p>Name: {{profile.name}}</p>
+    <p>Username: {{profile.username}}</p>
+    <p>Email: {{profile.email}}</p>
+    <p>Role: {{profile.role_id}}</p>
+  </div>`,
+
+  data() {
+    return {
+      profile: {
+        id: 0,
+        name: "Santhosh",
+        username: "santhosh",
+        email: "XXXXXXXXXXXXXXXXXX",
+        role_id: "usr"
+      },
+
+      success: true,
+      error: "Error",
+    }
+  },
+
+  async mounted() {
+    const response = await fetch('/api/user/0');
+    if (response.ok){
+      const data = await response.json();
+      this.profile = data;
+    }
+    else{
+      console.log("Error");
+    }
+    this.profile = response.data;
+  },
 }
 
 export default profile;
